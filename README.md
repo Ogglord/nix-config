88a8bf198c19fecc437e424193df21d4e1d13215
Ogglord <oag@proton.me>
2025-01-31 23:58:45 +0100

    restructure

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 ### nixos config 2025
+
+## Overview
+
+This repository contains my NixOS configuration, which is managed using Nix flakes. It includes a set of custom NixOS modules, home-manager configurations, and Nixpkgs overlays.
+
+## Getting Started
+
+### Prerequisites
+- Nix installed on your system
+- Nix flakes enabled
+- Git installed on your system
+
+### Installation
+
+
+### Daily Usage
+
+Update and apply system changes:
+
+```bash
+nh os switch
+```
+
+## Directory Structure
+
+    .
+    ├── hosts/              # Machine-specific configurations
+    │   └── default.nix     # Shared configuration between hosts
+    ├── modules/            # Custom NixOS modules
+    ├── home/               # Home-manager configurations
+    │   └── programs/       # Program-specific configurations
+    ├── overlays/           # Nixpkgs overlays
+    └── flake.nix          # Main system configuration
--- flake.nix
+++ flake.nix
@@ -34,90 +34,84 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    disko,
-    home-manager,
-    plasma-manager,
-    nix-environments,
-    nixos-cosmic,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    nixosConfigurations = {
-      # Using the hostname from configuration.nix
-      monsterdator = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {inherit inputs;};
-        modules = [
-          {
-            nix.settings = {
-              substituters = ["https://cosmic.cachix.org/"];
-              trusted-public-keys = ["cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="];
-            };
-          }
-          ./system/configuration.nix
-          nixos-cosmic.nixosModules.default
-          disko.nixosModules.disko
-          inputs.vscode-server.nixosModules.default
-          inputs.chaotic.nixosModules.default
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "bak";
-            home-manager.sharedModules = [
-              inputs.sops-nix.homeManagerModules.sops
-            ];
-            home-manager.users.ogge = {...}: {
-              imports = [
-                ./home-manager/home.nix
-                plasma-manager.homeManagerModules.plasma-manager
-                {
-                  nixpkgs.config.allowUnfree = true;
-                  nixpkgs.config.allowAliases = true;
-                }
-              ];
-            };
-          }
+  outputs = { self, nixpkgs, disko, home-manager, plasma-manager
+    , nix-environments, nixos-cosmic, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        # Using the hostname from configuration.nix
+        monsterdator = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            {
+              nix.settings = {
+                substituters = [ "https://cosmic.cachix.org/" ];
+                trusted-public-keys = [
+                  "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+                ];
+              };
+            }
+            ./system/configuration.nix
+            nixos-cosmic.nixosModules.default
+            disko.nixosModules.disko
+            inputs.vscode-server.nixosModules.default
+            inputs.chaotic.nixosModules.default
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "bak";
+              home-manager.sharedModules =
+                [ inputs.sops-nix.homeManagerModules.sops ];
+              home-manager.users.ogge = { ... }: {
+                imports = [
+                  ./home/home.nix
+                  plasma-manager.homeManagerModules.plasma-manager
+                  {
+                    nixpkgs.config.allowUnfree = true;
+                    nixpkgs.config.allowAliases = true;
+                  }
+                ];
+              };
+            }
+          ];
+        };
+      };
+
+      devShells.${system}.openwrt = pkgs.mkShell {
+        name = "openwrt-devshell";
+        buildInputs = with pkgs; [
+          bison
+          gnupg
+          go
+          libelf
+          llvmPackages_latest.llvm
+          ncdu
+          openssl
+          swig
+          quilt
+          squashfsTools
+          unzip
+          wget
+          zstd
+          ncurses
+          pkg-config
+          (python3.withPackages (ps: [ ps.distutils ps.pip ps.setuptools ]))
         ];
+        shellHook = ''
+          # Find the most recent LLVM library path
+          LLVM_HOST_PATH=${pkgs.llvmPackages_latest.llvm}/bin
+
+          # Export the LLVM host path
+          export LLVM_HOST_PATH
+
+          echo "OpenWrt development shell"
+          echo "LLVM Host Path: $LLVM_HOST_PATH"
+          echo "ncurses-dev Path: ${pkgs.ncurses.dev}"
+        '';
       };
     };
-
-    devShells.${system}.openwrt = pkgs.mkShell {
-      name = "openwrt-devshell";
-      buildInputs = with pkgs; [
-        bison
-        gnupg
-        go
-        libelf
-        llvmPackages_latest.llvm
-        ncdu
-        openssl
-        swig
-        quilt
-        squashfsTools
-        unzip
-        wget
-        zstd
-        ncurses
-        pkg-config
-        (python3.withPackages (ps: [ps.distutils ps.pip ps.setuptools]))
-      ];
-      shellHook = ''
-        # Find the most recent LLVM library path
-        LLVM_HOST_PATH=${pkgs.llvmPackages_latest.llvm}/bin
-
-        # Export the LLVM host path
-        export LLVM_HOST_PATH
-
-        echo "OpenWrt development shell"
-        echo "LLVM Host Path: $LLVM_HOST_PATH"
-        echo "ncurses-dev Path: ${pkgs.ncurses.dev}"
-      '';
-    };
-  };
 }
